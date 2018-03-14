3eca1220590bd73046aef848b7e6f8469ee01b85
aluno <Aluno@salas.aulas>
2018-03-13 21:38:31 -0300

    versao 1

---
+++ src/jogovelha.py
@@ -0,0 +1,18 @@
+# devops-aula05
+Aula 5
+
+def inicializar():
+    tab = []
+    for i in range(3):
+        linha = []
+        for j in range(3):
+            linha.append(".")
+        tab.append(linha)
+    return tab
+
+def main():
+    jogo = inicializar()
+    print(jogo)
+    
+if __name__ == "__main__":
+    main()
